217b65f81bba75f04ec2cfc53b1cbf09b4cc7df3
gileadekelvin <gileade.silva@ccc.ufcg.edu.br>
2017-02-16 14:13:34 -0300

    adding spinner

--- IFarma/app/src/main/java/com/ifarma/ifarma/fragments/AddProductFragment.java
+++ IFarma/app/src/main/java/com/ifarma/ifarma/fragments/AddProductFragment.java
@@ -1,11 +1,14 @@
 package com.ifarma.ifarma.fragments;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -14,8 +17,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ifarma.ifarma.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddProductFragment extends Fragment {
 
@@ -28,6 +35,7 @@ public class AddProductFragment extends Fragment {
     private EditText _indicacoesProdutoInput;
     private EditText _contraindicacoesProdutoInput;
     private CheckBox _genericoCheckbox;
+    private Spinner spinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,6 +53,8 @@ public class AddProductFragment extends Fragment {
         _indicacoesProdutoInput = (EditText) rootView.findViewById(R.id.input_indicacoes);
         _contraindicacoesProdutoInput = (EditText) rootView.findViewById(R.id.input_contra_indicacoes);
         _genericoCheckbox = (CheckBox) rootView.findViewById(R.id.checkbox_generico);
+
+        setUpSpinner();
 
         ImageView _backButton = (ImageView) rootView.findViewById(R.id.back_btn_cadastro_medicamento);
 
@@ -68,4 +78,32 @@ public class AddProductFragment extends Fragment {
 
         return rootView;
     }
+
+    private void setUpSpinner() {
+        spinner = (Spinner) rootView.findViewById(R.id.spinner);
+
+        List<String> pesos = new ArrayList<String>();
+        pesos.add("mg");
+        pesos.add("g");
+        pesos.add("ml");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getActivity(), R.layout.simplespinneritem, pesos);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+
+        AdapterView.OnItemSelectedListener adapter = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+
+        spinner.setOnItemSelectedListener(adapter);
+    }
+
 }
