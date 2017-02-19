42b5c4c4c0a6ad0c6e75e0143664ffbe8c0603f7
Rafael Albuquerque <rafael.sousa@splab.ufcg.edu.br>
2017-02-19 07:28:03 -0300

    Init branch

--- IFarma/app/src/main/java/com/ifarma/ifarma/fragments/RegisterFragment.java
+++ IFarma/app/src/main/java/com/ifarma/ifarma/fragments/RegisterFragment.java
@@ -22,6 +22,8 @@ public class RegisterFragment extends Fragment {
     private RecyclerView loginRecycler;
     private RecyclerView registerRecycler;
 
+    
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
